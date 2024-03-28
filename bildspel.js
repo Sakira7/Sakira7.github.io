4a642388c67e7fcda41701a114da1500174d37a4
Sara Elofsson <42539780+Sakira7@users.noreply.github.com>
2024-03-28 12:16:50 +0100

    Update bildspel.js

--- bildspel.js
+++ bildspel.js
@@ -13,7 +13,7 @@ function addFiles(){
             
             pic.setAttribute("src",files[i].name);
             pic.setAttribute("class","pic");
-            lcalStorage.setItem(pic);
+            lcalStorage.setItem(files[i].name);
             
             
         }
