a067939e3f9df356f960f9e8d712d78470e4f563
Sara Elofsson <42539780+Sakira7@users.noreply.github.com>
2024-04-23 16:25:01 +0200

    WORKING

--- bildspel.js
+++ bildspel.js
@@ -1,68 +1,87 @@
-function addFiles(){
+function logFile (event) {
+    let img_viewer = document.querySelector("#pics");
+    let str = event.target.result;
+    let img = document.createElement('img');
+    img.setAttribute("class", "pic");
+    localStorage.setItem("image"+ i, str);
+    img.src = localStorage.getItem("image"+i);
+    img_viewer.append(img);
     
-    let input = document.createElement('input');
-    input.type = 'file';
-
-    function encodeImageFileAsURL(element) {
-        var file = element.files[0];
-        var reader = new FileReader();
-        reader.onloadend = function() {
-          console.log('RESULT', reader.result)
-        }
-        reader.readAsDataURL(file); 
+  }
+  
+  
+  let i = 0;
+  window.addEventListener("load", ()=>{
+    console.log(localStorage);
+    let img_viewer = document.querySelector("#pics");
+  
+    for(var i = 0; i < localStorage.length; i++){
+      let img = document.createElement("img");
+      img.src = localStorage.getItem("image"+i);
+      img.setAttribute("class", "pic");
+      img_viewer.append(img);
     }
-
-    input.onchange = _ => {
-        
-        let img_viewer = document.getElementById("pics");
-        
-        let files = Array.from(input.files);
-
-        
-        for(var i=0;i<files.length;i++){
-            
-            localStorage.setItem("img"+i, files[i]);
-            let pic = document.createElement("img");
-            pic.setAttribute("class","pic");
-            pic.setAttribute("src", files[i].name);
-            img_viewer.append(pic);
-
-        }
-        
-        
+    
+    
+    
+  });
+  
+  
+  
+  function handleSubmit () {
+    
+    //localStorage.clear();
+    console.log(localStorage);
+    let file_picker = document.createElement("input");
+    file_picker.type = "file";
+    file_picker.accept = "image/*";
+    
+    
+    file_picker.onchange = (e)=>{
+      i=i+1;
+      e.preventDefault();
+      if (!file_picker.value.length) console.log("no file");
+  
+      let reader = new FileReader();
+  
+      reader.onload = logFile;
+  
+      reader.readAsDataURL(file_picker.files[0]);
+      console.log(i);
+  
     };
-    
-    input.click();
-    
-    
-    document.getElementById("begin").addEventListener("click", ()=>{
-
-        let pics = [];
-        pics = document.getElementById("pics").childNodes;
-        
-        
-        pics.forEach((pic)=>{
-            pic.removeAttribute("class", "pic");
-            pic.setAttribute("class","slider"); 
-            console.log(pic);
-        });
-        
-        
-        let header = document.getElementById("header");
-        let footer = document.getElementById("footer");
-        let buttons = document.getElementById("buttons");
-        let img_viewer = document.getElementById("pics");
-        
-        header.style.display = "none";
-        footer.style.display = "none";
-        buttons.style.display = "none";
-        img_viewer.style.top="0";
-
-
-    });
-    
-}
-
+    file_picker.click();
+   
+  
+      document.getElementById("begin").addEventListener("click", ()=>{
+  
+          let pics = [];
+          pics = document.getElementById("pics").childNodes;
+          console.log(localStorage);
+          
+          
+          pics.forEach((pic)=>{
+              pic.removeAttribute("class", "pic");
+              pic.setAttribute("class","slider"); 
+  
+          });
+          
+          
+          let header = document.getElementById("header");
+          let footer = document.getElementById("footer");
+          let buttons = document.getElementById("buttons");
+          let img_viewer = document.getElementById("pics");
+          
+          header.style.display = "none";
+          footer.style.display = "none";
+          buttons.style.display = "none";
+          img_viewer.style.top="0";
+  
+  
+      });
+  }
+      
+  
 
 
 
