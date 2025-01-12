e5dd1fd7ffe7dddadc12b1af8ceeeb3300a4e65c
Sara Elofsson <42539780+Sakira7@users.noreply.github.com>
2025-01-12 15:12:38 +0100

    changed names

--- drag.js
+++ drag.js
@@ -1,23 +1,11 @@
-$(document).ready(function() {
-    updateJP();
+function change(){
 
-    setInterval(function(){ updateJP(); }, 300000);
-});
+    let col = document.getElementsByTagName("input");
+    let val = col.length -1;
 
-function updateJP() {
-    $.getJSON("https://connect.idpit.se/jpinfo.php", {format: "json"}, function(){console.log("success")})
-    .done(function( data ) {
-      $("#super1").html("" + numberWithSpaces(data[0].rad1) + " kr");
-      $("#super2").html("" + numberWithSpaces(data[0].rad2) + " kr");
-      $("#super3").html("" + numberWithSpaces(data[0].rad3) + " kr");
-      $("#eldorado").html("" + numberWithSpaces(data[0].eldorado) + " kr");
-      $("#drom").html("" + numberWithSpaces(data[0].drom) + " kr");
-    })
-    .fail(function() {
-        console.log("Failed");
-    });
-}
 
-function numberWithSpaces(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    for(var i = 0; i < val; i++){
+
+        localStorage.setItem("value" + i, col[i].value);
+    }
 }
