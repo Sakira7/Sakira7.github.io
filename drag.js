d9bc1084f07e2e2810a0891e3a83d8e409958df1
Sara Elofsson <42539780+Sakira7@users.noreply.github.com>
2025-01-21 16:32:15 +0100

    changes dennis

--- drag.js
+++ drag.js
@@ -1,4 +1,14 @@
+let isOpen = false;
+let show;
 function change(){
+
+    if(isOpen === false){
+        show = window.open("show.html");
+        isOpen = true;
+    }
+    if(show.closed === true){
+        isOpen = false;
+    }
 
     let col = document.getElementsByTagName("input");
     let val = col.length;
@@ -8,4 +18,8 @@ function change(){
 
         localStorage.setItem("value" + i, col[i].value);
     }
+}
+function show_dp(){
+    document.getElementById("bd").style.visibility = "visible";
+    document.getElementById("bd1").style.visibility = "visible";
 }
