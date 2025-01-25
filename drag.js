065fdc76b03c327c4ce384a9ddb92aa307440f0e
Sara Elofsson <42539780+Sakira7@users.noreply.github.com>
2025-01-25 20:29:44 +0100

    tweaks

--- drag.js
+++ drag.js
@@ -4,12 +4,23 @@ let show;
 function update(){
 
     let col = document.getElementsByTagName("input");
-    let val = col.length;
+    let inputs = [];
+
+    for(var i = 0; i < col.length; i++){
+        if(col[i].value != ""){
+            inputs.push(col[i].value)
+        }
+    }
+
+    let val = inputs.length;
     isClicked = true;
+
+    
+    if(inputs.length > 0){
 
     for(var i = 0; i < val; i++){
 
-        localStorage.setItem("value" + i, col[i].value);
+        localStorage.setItem("value" + i, inputs[i]);
     }
 
     if(isOpen === false){
@@ -19,6 +30,24 @@ function update(){
     if(show.closed === true){
         isOpen = false;
     }
+
+
+    let date = new Date();
+    let day = date.getDay();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    if(minutes.length === 1){
+        minutes = "0" + minutes;
+    }
+
+    let days = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag"];
+
+    let show_date = document.getElementById("last_updt");
+    show_date.innerHTML = "Senast uppdaterad: " + days[day-1] + " " + hours + ":" + minutes;
+}else{
+    alert("Fyll i alla fält!");
+}
     
 }
 window.addEventListener("beforeunload",()=>{
