8390ace7ef6bde8f9381d09d22871c3fbc77e75b
Sara Elofsson <42539780+Sakira7@users.noreply.github.com>
2025-02-14 15:42:22 +0100

    drag

--- drag.js
+++ drag.js
@@ -18,42 +18,75 @@ function update(){
     
     if(inputs.length > 0){
 
-    for(var i = 0; i < val; i++){
+        for(var i = 0; i < val; i++){
 
-        localStorage.setItem("value" + i, inputs[i]);
+            localStorage.setItem("value" + i, inputs[i]);
+        }
+
+        if(isOpen === false){
+            show = window.open("show.html");
+            isOpen = true;
+        }
+        if(show.closed === true){
+            isOpen = false;
+        }
+
+        let date = new Date();
+        let day = date.getDay();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+
+        if(minutes.toString().length === 1){
+            minutes = "0" + minutes;
+        }
+        if(hours.toString().length === 1){
+            hours = "0" + hours;
+        }
+
+        let days = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"];
+
+        let show_date = document.getElementById("last_updt");
+        show_date.innerHTML = "Senast uppdaterad: " + "<span class='bold'>"+days[day] + " " + hours + ":" + minutes+"</span>";
+    }else{
+        document.getElementById("pop-up").style.display="block";
+        document.getElementById("ok").addEventListener("click", ()=>{
+            document.getElementById("pop-up").style.display="none";
+        });
+
+        document.addEventListener("keydown", (e)=>{
+            if(e.key === "Enter"){
+                document.getElementById("pop-up").style.display="none";
+            }
+        });
     }
-
-    if(isOpen === false){
-        show = window.open("show.html");
-        isOpen = true;
-    }
-    if(show.closed === true){
-        isOpen = false;
-    }
-
-
-    let date = new Date();
-    let day = date.getDay();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    
-
-
-    if(minutes.toString().length === 1){
-        minutes = "0" + minutes;
-    }
-
-    let days = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"];
-
-    let show_date = document.getElementById("last_updt");
-    show_date.innerHTML = "Senast uppdaterad: " + "<span class='bold'>"+days[day] + " " + hours + ":" + minutes+"</span>";
-}else{
-    alert("Fyll i alla fält!");
-}
-    
 }
 window.addEventListener("beforeunload",()=>{
     show.close();
-})
+});
+
+let i = 0;
+setInterval(() => {
+    i++
+    if(i == 60){
+        document.getElementById("pop-up").style.display = "block";
+        document.getElementById("pop-up-text").innerHTML = "Dags att uppdatera dragen!";
+
+        document.getElementById("ok").addEventListener("click", ()=>{
+            document.getElementById("pop-up").style.display="none";
+            i=0;
+        });
+
+        document.addEventListener("keydown", (e)=>{
+            if(e.key === "Enter"){
+                document.getElementById("pop-up").style.display="none";
+                i=0;
+            }
+        });
+        
+    }
+
+}, 60000);
+
+
 
 
